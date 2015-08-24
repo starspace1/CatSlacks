f7a295948426b3e3ba0fe0516084dfe61a9ba76c
Leslie Brown <lkb@alum.mit.edu>
2015-08-24 15:42:35 -0400

    adding navbar to log in / out with slack

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -8,6 +8,8 @@ class OauthController < ApplicationController
 
   def deauthorize
     session[:access_token] = nil
+    session[:team] = nil
+    session[:user] = nil
     redirect_to root_url, notice: 'You have logged out of Slack.'
   end
 
@@ -18,7 +20,10 @@ class OauthController < ApplicationController
                                   site: SLACK[:authorize_uri],
                                   token_url: SLACK[:token_uri])
       session[:access_token] = client.auth_code.get_token(params[:code], redirect_uri: SLACK[:redirect_uri]).token
-      redirect_to root_url, notice: 'You can now post to Slack from this app.'
+      response = HTTParty.get('https://slack.com/api/auth.test', query: { token: session[:access_token] })
+      session[:team] = response.parsed_response["team"]
+      session[:user] = response.parsed_response["user"]
+      redirect_to root_url, notice: 'Successfully logged in with Slack!'
     else
       redirect_to root_url, notice: "Fail. #{params}"
     end
