14d5611ff51cb9523ea8ed8f5c4eb95c23bc9be7
Leslie Brown <lkb@alum.mit.edu>
2015-08-24 13:45:31 -0400

    storing redirect uri using dot_env

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -2,6 +2,6 @@ SLACK = {
   client_id: ENV["SLACK_CLIENT_ID"],
   client_secret: ENV["SLACK_CLIENT_SECRET"],
   authorize_uri: "https://slack.com/oauth/authorize",
-  redirect_uri: "http://localhost:3000/oauth/request_access_token",
+  redirect_uri: ENV["SLACK_REDIRECT_URI"],
   token_uri: "https://slack.com/api/oauth.access"
 }
