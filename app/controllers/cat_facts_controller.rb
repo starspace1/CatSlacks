8e13a2f063d3fb90ca710048b64196e0bd5cc926
Leslie Brown <lkb@alum.mit.edu>
2015-08-24 12:36:53 -0400

    user can now choose a channel to send a cat fact to

--- app/controllers/cat_facts_controller.rb
+++ app/controllers/cat_facts_controller.rb
@@ -6,6 +6,10 @@ class CatFactsController < ApplicationController
     response = HTTParty.get('http://catfacts-api.appspot.com/api/facts?number=1')
     cat_hash = JSON.parse(response)
     @fact = cat_hash['facts'][0]
+    if session[:access_token]
+      response = HTTParty.get('https://slack.com/api/channels.list', query: { token: session[:access_token] })
+      @channels = response.parsed_response["channels"]
+    end
   end
 
   def send_message
@@ -13,14 +17,12 @@ class CatFactsController < ApplicationController
     if session[:access_token].nil?
       redirect_to root_url, notice: "You can't send a CAT FACT until you authorize Slack."
     else
-      # TODO: present a list of channels for user to select from
-      # C04M51FHL is front-end, C06VD893L is orl, C04U2G8SM is rails
       HTTParty.post('https://slack.com/api/chat.postMessage', query: {
-                      token: session[:access_token],
-                      channel: 'C04U2G8SM',
-                      text: 'CAT FACT! ' + params[:fact],
-                      username: 'CatBot',
-                      icon_url: 'http://lorempixel.com/g/100/100/cats/' })
+        token: session[:access_token],
+        channel: params[:channel_id],
+        text: 'CAT FACT! ' + params[:fact],
+        username: 'CatBot',
+        icon_url: 'http://lorempixel.com/g/100/100/cats/' })
       redirect_to root_url, notice: 'Success! You sent a CAT FACT!'
     end
   end
